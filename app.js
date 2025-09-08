30906880b7bbd344d58cb5949b3129a17c0dd75f
Pasquale <137796364+mrme77@users.noreply.github.com>
2025-09-07 21:10:09 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const chatBody = document.getElementById('chatBody');
 const submitBtn = document.getElementById('submitBtn');
 
 // Your Vercel backend endpoint
-const API_URL = 'https://personal-goal-assistant-back-end.vercel.app/api/agent';
+//const API_URL = 'https://personal-goal-assistant-back-end.vercel.app/api/agent';
+const API_URL = 'https://mrme77.github.io/Personal-Goals-Assistant/personal-goal-assistant-back-end.vercel.app';
+
 //const API_URL = 'http://localhost:5000/api/agent';
 
 form.addEventListener('submit', async (ev) => {
