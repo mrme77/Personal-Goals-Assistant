f2f63bfc542ff165ee18fc55abb26b90293104fb
Pasquale <137796364+mrme77@users.noreply.github.com>
2025-09-07 22:10:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://personal-goal-assistant-git-3c7e7e-pasquales-projects-5c7ff561.vercel.app';
+const API_URL = 'https://personal-goal-assistant-back-end.vercel.app';
 
 const form = document.getElementById('goalForm');
 const goalInput = document.getElementById('goalInput');
